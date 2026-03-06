59c96a99e17d622f73d89ebd7b2f3dc4b64b87cd
DieAllPies <dennisfamiliapedersen@gmail.com>
2026-03-06 11:27:29 -0500

    Implement List Accounts endpoint and tests

--- service/routes.py
+++ service/routes.py
@@ -60,8 +60,19 @@ def create_accounts():
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
+@app.route("/accounts", methods=["GET"])
+def list_accounts():
+    """
+    List all Accounts
+    This endpoint will list all Accounts
+    """
+    app.logger.info("Request to list Accounts")
 
+    accounts = Account.all()
+    account_list = [account.serialize() for account in accounts]
 
+    app.logger.info("Returning [%s] accounts", len(account_list))
+    return jsonify(account_list), status.HTTP_200_OK
 
 ######################################################################
 # READ AN ACCOUNT
@@ -79,6 +90,14 @@ def get_accounts(account_id):
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
 
     return account.serialize(), status.HTTP_200_OK
+
+######################################################################
+# UPDATE AN ACCOUNT
+######################################################################
+
+######################################################################
+# DELETE AN ACCOUNT
+######################################################################
 
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
